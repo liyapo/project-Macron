245903434c91f2a1040ad73780d6410bb2fbd7fb
liyapo <liyapo@mail.ru>
2017-12-27 11:15:19 +0100

    last line corrected

--- MacronFollowers.py
+++ MacronFollowers.py
@@ -5,6 +5,6 @@ htmlpage = html.fromstring(pagestring.content)
 
 #Find class a with attribute 'followers' and access its number
 fllw = htmlpage.xpath("//a[@data-nav='followers']/span/@data-count")
-print(fllw)
+print(fllw[0])
 
 
